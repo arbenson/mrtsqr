0935e792fdb80c73a260f97b7f01d795ce59e7ef
Austin Benson <arbenson@berkeley.edu>
2011-11-30 18:23:32 -0800

    sample 2-step job launch

---
+++ cxx/job.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+STREAMING_JAR=/usr/common/tig/hadoop/hadoop-0.20.2+228/contrib/streaming/hadoop-0.20.2+228-streaming.jar
+
+hadoop fs -rmr cpp-test-qrr.mseq
+
+hadoop jar $STREAMING_JAR -input $1 -output cpp-test-qrr.mseq -numReduceTasks $2 \
+  -jobconf mapreduce.job.name=tsqr_cxx \
+  -jobconf 'stream.map.input=typedbytes' -jobconf 'stream.reduce.input=typedbytes' \
+  -jobconf 'stream.map.output=typedbytes' -jobconf 'stream.reduce.output=typedbytes' \
+  -jobconf 'mapred.child.java.opts=-Xmx2G' \
+  -outputformat 'org.apache.hadoop.mapred.SequenceFileOutputFormat' \
+  -inputformat 'org.apache.hadoop.streaming.AutoInputFormat' \
+  -io typedbytes \
+  -file mkl_and_tsqr.sh \
+  -file /usr/common/usg/mkl/10.2.2.025/lib/em64t/libmkl_core.so \
+  -file /usr/common/usg/mkl/10.2.2.025/lib/em64t/libmkl_intel_lp64.so \
+  -file /usr/common/usg/mkl/10.2.2.025/lib/em64t/libmkl_sequential.so \
+  -file /usr/common/usg/mkl/10.2.2.025/lib/em64t/libiomp5.so \
+  -file tsqr \
+  -mapper './mkl_and_tsqr.sh map 5 4 house 0' -reducer './mkl_and_tsqr.sh reduce 5 4 house 0'
+
+hadoop fs -rmr cpp-test-house.mseq
+
+hadoop jar $STREAMING_JAR -input cpp-test-qrr.mseq -output cpp-test-house.mseq -numReduceTasks 1 \
+  -jobconf mapreduce.job.name=tsqr_cxx \
+  -jobconf 'stream.map.input=typedbytes' -jobconf 'stream.reduce.input=typedbytes' \
+  -jobconf 'stream.map.output=typedbytes' -jobconf 'stream.reduce.output=typedbytes' \
+  -jobconf 'mapred.child.java.opts=-Xmx2G' \
+  -outputformat 'org.apache.hadoop.mapred.SequenceFileOutputFormat' \
+  -inputformat 'org.apache.hadoop.streaming.AutoInputFormat' \
+  -io typedbytes \
+  -file mkl_and_tsqr.sh \
+  -file /usr/common/usg/mkl/10.2.2.025/lib/em64t/libmkl_core.so \
+  -file /usr/common/usg/mkl/10.2.2.025/lib/em64t/libmkl_intel_lp64.so \
+  -file /usr/common/usg/mkl/10.2.2.025/lib/em64t/libmkl_sequential.so \
+  -file /usr/common/usg/mkl/10.2.2.025/lib/em64t/libiomp5.so \
+  -file tsqr \
+  -mapper './mkl_and_tsqr.sh map 5 4 house 1' -reducer './mkl_and_tsqr.sh reduce 5 4 house 1'
