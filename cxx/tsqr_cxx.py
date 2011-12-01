165871031bdd52ade8379bcc4363ebce4d317cb3
Austin Benson <arbenson@berkeley.edu>
2011-11-30 18:26:10 -0800

    tweaks for NERSC Hadoop

--- cxx/tsqr_cxx.py
+++ cxx/tsqr_cxx.py
@@ -40,8 +40,11 @@ def get_args(argv):
     return args
 
 if __name__=='__main__':
-    hadoop_dir = '/home/mrhadoop/hadoop-0.21.0/'
-    streaming_jar = hadoop_dir + 'mapred/contrib/streaming/hadoop-0.21.0-streaming.jar'
+    #hadoop_dir = '/home/mrhadoop/hadoop-0.21.0/'
+    #streaming_jar = hadoop_dir + 'mapred/contrib/streaming/hadoop-0.21.0-streaming.jar'
+
+    hadoop_dir = '/usr/common/tig/hadoop/hadoop-0.20.2+228/'
+    streaming_jar = hadoop_dir + 'contrib/streaming/hadoop-0.20.2+228-streaming.jar'
     
     args = get_args(sys.argv[1:])
     
@@ -51,7 +54,7 @@ if __name__=='__main__':
         mat = args['mat']
     else:
         print >>sys.stderr, "Error: -mat not specified"
-        
+
     input = mat
     matname,matext = os.path.splitext(mat)
     
@@ -59,6 +62,12 @@ if __name__=='__main__':
         output = args['output']
     else:
         output = '%s-qrr%s'%(matname,matext)
+
+    if 'rows_per_rec' in args:
+        rows_per_rec = args['rows_per_rec']
+    else:
+        rows_per_rec = 1
+
         
     jobname = 'tsqr_cxx ' + matname
     
@@ -79,8 +88,13 @@ if __name__=='__main__':
     
     hadoop_args.extend(['-io', 'typedbytes'])
     hadoop_args.extend(['-file', 'tsqr'])
-    hadoop_args.extend(['-reducer', "'./tsqr reduce %i'"%(blocksize)])
-    #hadoop_args.extend(['-combiner', "'./tsqr reduce %i'"%(blocksize)])
+    hadoop_args.extend(['-file', '/usr/common/usg/mkl/10.2.2.025/lib/em64t/libmkl_core.so'])
+    hadoop_args.extend(['-file', '/usr/common/usg/mkl/10.2.2.025/lib/em64t/libmkl_intel_lp64.so'])
+    hadoop_args.extend(['-file', '/usr/common/usg/mkl/10.2.2.025/lib/em64t/libmkl_sequential.so'])
+    hadoop_args.extend(['-file', '/usr/common/usg/mkl/10.2.2.025/lib/em64t/libiomp5.so'])
+    hadoop_args.extend(['-file', 'mkl_and_tsqr.sh'])
+    hadoop_args.extend(['-reducer', "'./mkl_and_tsqr.sh reduce %i'"%(blocksize)])
+    #hadoop_args.extend(['-combiner', "'./mkl_and_tsqr.sh reduce %i'"%(blocksize)])
     hadoop_args.extend(['-outputformat', "'org.apache.hadoop.mapred.SequenceFileOutputFormat'"])
     hadoop_args.extend(['-inputformat', "'org.apache.hadoop.streaming.AutoInputFormat'"])
 
@@ -96,7 +110,7 @@ if __name__=='__main__':
             input = curoutput
             cur_args.extend(['-mapper', "'org.apache.hadoop.mapred.lib.IdentityMapper'"])    
         else:
-            cur_args.extend(['-mapper', "'./tsqr map %i'"%(blocksize)])    
+            cur_args.extend(['-mapper', "'./mkl_and_tsqr.sh map %i'"%(blocksize)])    
         
         if i+1==len(steps):
             curoutput = output
@@ -124,5 +138,3 @@ if __name__=='__main__':
             hadoopy.rm(curoutput)
 
         subprocess.check_call(' '.join(cmd),shell=True)
-
-
