1681c8886942ed1d9a02078b9f81187c07f9789c
Austin Benson <arbenson@berkeley.edu>
2012-03-23 20:20:49 -0700

    fix cholesky and take out boilerplate code

--- dumbo/CholeskyQR.py
+++ dumbo/CholeskyQR.py
@@ -20,6 +20,7 @@ import numpy
 import numpy.linalg
 
 import util
+import base
 
 import dumbo
 import dumbo.backends.common
@@ -29,12 +30,9 @@ gopts = util.GlobalOptions()
 
 class Cholesky(dumbo.backends.common.MapRedBase):
     def __init__(self,ncols=10):
-        self.data = range(ncols)
         self.ncols = ncols
+        self.data = [numpy.zeros((1, ncols)) for x in range(ncols)]
     
-    def array2list(self,row):
-        return [float(val) for val in row]
-
     def close(self):
         L = numpy.linalg.cholesky(self.data)
         M = numpy.mat(L.T)
@@ -44,37 +42,20 @@ class Cholesky(dumbo.backends.common.MapRedBase):
     def __call__(self,data):
         for key,values in data:
             for value in values:
-                self.data[key] = list(struct.unpack('d'*self.ncols, value))
+                self.data[key] += numpy.array(list(struct.unpack('d'*self.ncols, value)))
                 
         for key,val in self.close():
             yield key, val
 
-class AtA(dumbo.backends.common.MapRedBase):
-    def __init__(self,blocksize=3,keytype='random',isreducer=False,ncols=10):
+class AtA(base.MatrixHandler):
+    def __init__(self,blocksize=3,isreducer=False,ncols=10):
         self.blocksize=blocksize
-        if keytype=='random':
-            self.keyfunc = lambda x: random.randint(0, 4000000000)
-        elif keytype=='first':
-            self.keyfunc = self._firstkey
-        else:
-            raise Error("Unkonwn keytype %s"%(keytype))
-        self.first_key = None
         self.isreducer=isreducer
         self.nrows = 0
         self.data = []
-        self.ncols = ncols
         self.A_curr = None
         self.row = None
     
-    def _firstkey(self, i):
-        if isinstance(self.first_key, (list,tuple)):
-            return (util.flatten(self.first_key),i)
-        else:
-            return (self.first_key,i)
-    
-    def array2list(self,row):
-        return [float(val) for val in row]
-
     def compress(self):
         # Compute AtA on the data accumulated so far
         if self.ncols is None:
@@ -97,17 +78,10 @@ class AtA(dumbo.backends.common.MapRedBase):
 
     
     def collect(self,key,value):
-        if len(self.data) == 0:
-            self.first_key = key
-        
         if self.ncols == None:
             self.ncols = len(value)
             print >>sys.stderr, "Matrix size: %i columns"%(self.ncols)
         else:
-            # TODO should we warn and truncate here?
-            # No. that seems like something that will introduce
-            # bugs.  Maybe we could add a "liberal" flag
-            # for that.
             if not len(value) == self.ncols:
                 return
         
@@ -128,13 +102,22 @@ class AtA(dumbo.backends.common.MapRedBase):
         self.compress()
         if self.A_curr is not None:
             for ind, row in enumerate(self.A_curr.getA()):
-                r = self.array2list(row)
+                r = util.array2list(row)
                 yield ind, struct.pack('d'*len(r),*r)
 
             
     def __call__(self,data):
+        deduced = False
         if self.isreducer == False:
             # map job
+            if isinstance(value, str):
+                if not deduced:
+                    deduced = self.deduce_string_type(value)
+                # handle conversion from string
+                if self.unpacker is not None:
+                    value = self.unpacker.unpack(value)
+                else:
+                    value = [float(p) for p in value.split()]            
             for key,value in data:
                 value = list(struct.unpack('d'*self.ncols, value))
                 self.collect(key,value)
@@ -166,8 +149,7 @@ def runner(job):
         if part.startswith('s'):
             nreducers = int(part[1:])
             # these tasks should just spray data and compress
-            job.additer(mapper="org.apache.hadoop.mapred.lib.IdentityMapper",
-                reducer="org.apache.hadoop.mapred.lib.IdentityReducer",
+            job.additer(mapper = base.ID_MAPPER, reducer = base.ID_REDUCER,
                 opts=[('numreducetasks',str(nreducers))])
             job.additer(mapper, reducer, opts=[('numreducetasks',str(nreducers))])
             
@@ -177,61 +159,26 @@ def runner(job):
                 mapper = AtA(blocksize=blocksize,isreducer=False,ncols=ncols)
                 reducer = AtA(blocksize=blocksize,isreducer=True,ncols=ncols)
             else:
-                mapper = 'org.apache.hadoop.mapred.lib.IdentityMapper'
+                mapper = base.ID_MAPPER
                 reducer = Cholesky(ncols=ncols)
             job.additer(mapper=mapper, reducer=reducer, opts=[('numreducetasks',str(nreducers))])
-    
 
 def starter(prog):
-    
-    print "running starter!"
-    
-    mypath =  os.path.dirname(__file__)
-    print "my path: " + mypath
-    
-    # set the global opts
+    # set the global opts    
     gopts.prog = prog
-    
-   
-    mat = prog.delopt('mat')
-    if not mat:
-        return "'mat' not specified'"
-        
-    prog.addopt('memlimit','2g')
-    
-    nonumpy = prog.delopt('use_system_numpy')
-    if nonumpy is None:
-        print >> sys.stderr, 'adding numpy egg: %s'%(str(nonumpy))
-        prog.addopt('libegg', 'numpy')
-        
-    prog.addopt('file',os.path.join(mypath,'util.py'))
-
-
-    numreps = prog.delopt('replication')
-    if not numreps:
-        numreps = 1
-    for i in range(int(numreps)):
-        prog.addopt('input',mat)
-
-    #prog.addopt('input',mat)
-    matname,matext = os.path.splitext(mat)
     
     gopts.getintkey('blocksize',3)
     gopts.getstrkey('reduce_schedule','1')
-    gopts.getintkey('ncols', -1)
+    gopts.getintkey('ncols', -1)    
+
+    mat = base.starter_helper(prog)
+    if not mat: return "'mat' not specified"
     
+    matname,matext = os.path.splitext(mat)
     output = prog.getopt('output')
     if not output:
         prog.addopt('output','%s-chol-qrr%s'%(matname,matext))
-        
-    splitsize = prog.delopt('split_size')
-    if splitsize is not None:
-        prog.addopt('jobconf',
-            'mapreduce.input.fileinputformat.split.minsize='+str(splitsize))
-        
-    prog.addopt('overwrite','yes')
-    prog.addopt('jobconf','mapred.output.compress=true')
-    
+
     gopts.save_params()
 
 if __name__ == '__main__':
