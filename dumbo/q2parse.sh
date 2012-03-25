1d9bbc10cfde281d6ef81736ff1b5444cd51adf7
Austin Benson <arbenson@berkeley.edu>
2012-03-25 10:44:00 -0700

    put two delete calls into one

--- dumbo/q2parse.sh
+++ dumbo/q2parse.sh
@@ -4,10 +4,9 @@ rm -rf $1_tmp*
 
 tr ",\n" ", " < $1 > $1_tmp1
 tr ")" "\n" < $1_tmp1 > $1_tmp3
-tr -d "]" < $1_tmp3 > $1_tmp4
-tr -d "[" < $1_tmp4 > $1_tmp5
+tr -d "[\[\]]" < $1_tmp3 > $1_tmp4
 
-mv $1_tmp5 $1
+mv $1_tmp4 $1
 
 rm -rf $1_tmp*
 
