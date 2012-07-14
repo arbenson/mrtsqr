1ba6fe95e747ef454fb1b3fe93d1a5aef52340ca
arbenson <arbenson@berkeley.edu>
2012-07-14 10:42:24 -0700

    local directory for run_full_tsqr.py local output

--- dumbo/run_full_tsqr.py
+++ dumbo/run_full_tsqr.py
@@ -10,6 +10,7 @@ See options:
 Example usage:
      python run_full_tsqr.py --input=A_800M_10.bseq \
             --ncols=10 --svd=2 --schedule=100,100,100 \
+            --hadoop=icme-hadoop1 --local_output=tsqr-tmp \
             --output=FULL_TESTING
 
 This script is designed to run on ICME's MapReduce cluster, icme-hadoop1.
@@ -20,6 +21,7 @@ Copyright (c) 2012
 """
 
 import os
+import shutil
 import subprocess
 import sys
 import time
@@ -35,6 +37,8 @@ parser.add_option('-i', '--input', dest='input', default='',
                   help='input matrix')
 parser.add_option('-o', '--output', dest='out', default='',
                   help='base string for output of Hadoop jobs')
+parser.add_option('-l', '--local_output', dest='local_out', default='full_out_tmp',
+                  help='Base directory for placing local files')
 parser.add_option('-n', '--ncols', type='int', dest='ncols', default=0,
                   help='number of columns in the matrix')
 parser.add_option('-s', '--schedule', dest='sched', default='100,100,100',
@@ -68,6 +72,12 @@ if out == '':
   # TODO(arbenson): make sure in1 is clean
   out = in1 + '_FULL'
 
+local_out = options.local_out
+out_file = lambda f: local_out + '/' + f
+if os.path.exists(local_out):
+  shutil.rmtree(local_out)
+os.mkdir(local_out)
+
 ncols = options.ncols
 if ncols == 0:
   cm.error('number of columns not provided, use --ncols')
@@ -97,7 +107,7 @@ cm.run_dumbo('full2.py', hadoop, ['-mat ' + out1 + '/R_*', '-output ' + out2,
                                   '-libjar feathers.jar'])
 
 # Q2 file needs parsing before being distributed to phase 3
-Q2_file = out2 + '_Q2.txt'
+Q2_file = out_file('Q2.txt')
 
 if os.path.exists(Q2_file):
   os.remove(Q2_file)
@@ -116,7 +126,7 @@ cm.run_dumbo('full3.py', hadoop, ['-mat ' + in3, '-output ' + out + '_3',
                                   '-libjar feathers.jar'])
 
 if svd_opt == 2:
-  small_U_file = out2 + '_U.txt'
+  small_U_file = out_file('U.txt')
 
   if os.path.exists(small_U_file):
     os.remove(small_U_file)
