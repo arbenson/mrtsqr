07a22bbbea0f98d44357f6ed9ca42371b855146d
arbenson <arbenson@berkeley.edu>
2012-07-14 10:41:01 -0700

    use new CommandManager in form_Y

--- dumbo/form_Y.py
+++ dumbo/form_Y.py
@@ -19,34 +19,17 @@ consisting of the real pats and imaginary parts of the elements of Y.
 
 Copyright (c) 2012
 Austin R. Benson arbenson@gmail.com
+David F. Gleich
 '''
 
-import os
-import sys
-import numpy
-import subprocess
 from optparse import OptionParser
+import numpy
+import os
+import subprocess
+import sys
+import util
 
-# print error messages and exit with failure
-def error(msg):
-  print msg
-  sys.exit(1)
-
-# simple wrapper around printing with verbose option
-def output(msg):
-  print msg
-
-# simple wrapper for parsing a sequence file
-def parse_seq_file(inp):
-  parse_cmd = 'python hyy-python-hadoop/examples/SequenceFileReader.py %s > %s' % (inp, inp + '.out')
-  exec_cmd(parse_cmd)
-
-# simple wrapper for executing command line programs
-def exec_cmd(cmd):
-  output('(command is: %s)' % (cmd))
-  retcode = subprocess.call(cmd,shell=True)
-  # TODO(arbenson): make it more obvious when something fails
-  return retcode
+cm = util.CommandManager()
 
 parser = OptionParser()
 parser.add_option('-s', '--S', dest='S', default='',
@@ -62,51 +45,34 @@ parser.add_option('-o', '--output', dest='out', default='DMD_Y',
 matrices = {}
 matrices['S'] = {'path': options.S, 'mat': None}
 if matrices['S']['path'] == '':
-    error('no S matrix provided, use --S')
+    cm.error('no S matrix provided, use --S')
 
 matrices['Vt'] = {'path': options.Vt, 'mat': None}
 if matrices['Vt']['path'] == '':
-    error('no V^t matrix provided, use --Vt')
+    cm.error('no V^t matrix provided, use --Vt')
 
 matrices['Sigma'] = {'path': options.Sigma, 'mat': None}
 if matrices['Sigma']['path'] == '':
-    error('no Sigma matrix provided, use --Sigma')
+    cm.error('no Sigma matrix provided, use --Sigma')
 out = options.out
 
 for matrix in matrices:
-    # remove local file if it exists
-    local_store = 'form_Y_' + matrix + '.txt'
-    if os.path.exists(local_store):
-        os.remove(local_store)
+    # copy the data over and parse it
+    local_store = 'form_Y_' + matrix
+    cm.copy_from_hdfs(matrices[matrix]['path'], local_store + '.mseq')
+    cm.parse_seq_file(local_store + '.mseq', local_store + '.txt')
 
-    # copy the data locally
-    copy_cmd = 'hadoop fs -copyToLocal %s/part-00000 %s' % (matrices[matrix]['path'], local_store)
-    exec_cmd(copy_cmd)
-
-    # read the data
-    parse_seq_file(local_store)
-
-    f = open(local_store + '.out', 'r')
+    # read the local data
     data = []
-    for line in f:
-      if len(line) > 5:
-        ind2 = line.rfind(')')
-        line = line[ind2+3:]
-        line = line.lstrip('[').rstrip().rstrip(']')
-        try:
-          line2 = line.split(',')
-          line2 = [float(v) for v in line2]
-        except:
-          line2 = line.split()
-          line2 = [float(v) for v in line2]
-          data.append(line2)
-    f.close()
+    for line in util.parse_matrix_txt(local_store + '.txt'):
+      data.append(line)
+
     matrices[matrix]['mat'] = numpy.mat(data)
 
 S = matrices['S']['mat']
 V = numpy.transpose(matrices['Vt']['mat'])
 Sigma_inv = numpy.linalg.pinv(matrices['Sigma']['mat'])
-S_tilde = S*V*Sigma_inv
+S_tilde = S * V * Sigma_inv
 eig_vals, Y = numpy.linalg.eig(S_tilde)
 Y_Re = numpy.real(Y)
 Y_Im = numpy.imag(Y)
