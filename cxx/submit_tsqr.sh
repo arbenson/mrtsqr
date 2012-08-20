d991298580738554cf99048958e47161275bf2b7
arbenson <arbenson@berkeley.edu>
2012-08-20 13:50:29 -0700

    changes for icme-hadoop1

--- cxx/submit_tsqr.sh
+++ cxx/submit_tsqr.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-HADOOP_DIR=/home/mrhadoop/hadoop-0.21.0
-STREAMING_JAR=$HADOOP_DIR/mapred/contrib/streaming/hadoop-0.21.0-streaming.jar
+#HADOOP_DIR=/home/mrhadoop/hadoop-0.21.0
+#STREAMING_JAR=$HADOOP_DIR/mapred/contrib/streaming/hadoop-0.21.0-streaming.jar
+STREAMING_JAR=/usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2-cdh3u4.jar
 
 hadoop fs -rmr test-sumsq
 
-hadoop jar $STREAMING_JAR -input test.mseq -output test-sumsq \
+hadoop jar $STREAMING_JAR -input .mseq -output test-sumsq \
   -jobconf mapreduce.job.name=tsqr_cxx \
-  -jobconf 'stream.map.input=typedbytes' -jobconf 'stream.reduce.input=typedbytes' \
-  -jobconf 'stream.map.output=typedbytes' -jobconf 'stream.reduce.output=typedbytes' \
+  -jobconf 'stream.map.input=typedbytes' \
+  -jobconf 'stream.reduce.input=typedbytes' \
+  -jobconf 'stream.map.output=typedbytes' \
+  -jobconf 'stream.reduce.output=typedbytes' \
   -outputformat 'org.apache.hadoop.mapred.SequenceFileOutputFormat' \
   -inputformat 'org.apache.hadoop.streaming.AutoInputFormat' \
-  -file tsqr -mapper "./tsqr map" -reducer 'org.apache.hadoop.mapred.lib.IdentityReducer' \
+  -file tsqr \
+  -file tsqr_wrapper.sh \
+  -file libblas.so.3 \
+  -mapper "./tsqr_wrapper.sh map" \
+  -reducer 'org.apache.hadoop.mapred.lib.IdentityReducer' \
 
 hadoop jar $STREAMING_JAR dumptb test-sumsq | ./dump_typedbytes_info -  
