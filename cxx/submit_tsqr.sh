9a19f480439b68e19ebb0e91eb7d301c3362b786
arbenson <arbenson@berkeley.edu>
2012-08-21 19:19:54 -0700

    use feathers

--- cxx/submit_tsqr.sh
+++ cxx/submit_tsqr.sh
@@ -2,17 +2,21 @@
 
 #HADOOP_DIR=/home/mrhadoop/hadoop-0.21.0
 #STREAMING_JAR=$HADOOP_DIR/mapred/contrib/streaming/hadoop-0.21.0-streaming.jar
+#  -outputformat 'org.apache.hadoop.mapred.SequenceFileOutputFormat' \
 STREAMING_JAR=/usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2-cdh3u4.jar
 
 hadoop fs -rmr test-sumsq
 
-hadoop jar $STREAMING_JAR -input B_10.mseq -output test-sumsq \
+hadoop jar $STREAMING_JAR \
+  -libjars 'feathers.jar' \
+  -input B_10.mseq \
+  -output test-sumsq \
   -jobconf mapreduce.job.name=tsqr_cxx \
   -jobconf 'stream.map.input=typedbytes' \
   -jobconf 'stream.reduce.input=typedbytes' \
   -jobconf 'stream.map.output=typedbytes' \
   -jobconf 'stream.reduce.output=typedbytes' \
-  -outputformat 'org.apache.hadoop.mapred.SequenceFileOutputFormat' \
+  -outputformat 'fm.last.feathers.output.MultipleTextFiles' \
   -inputformat 'org.apache.hadoop.streaming.AutoInputFormat' \
   -file tsqr \
   -file tsqr_wrapper.sh \
