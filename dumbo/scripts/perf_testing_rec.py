2f4f47c500f99779a59fa3870c6b07c5d81f8961
arbenson <arbenson@berkeley.edu>
2013-07-27 10:02:11 -0700

    Script for running recursive performance tests.

---
+++ dumbo/scripts/perf_testing_rec.py
@@ -0,0 +1,21 @@
+import util, sys
+cm = util.CommandManager()
+
+matrices = [('A_500M_50.bseq', 2000, 10), ('A_4B_4.bseq', 2000, 400),
+            ('A_2500M_10.bseq', 2600, 200), ('A_150M_100.bseq', 1400, 10),
+            ('A_800M_10.bseq', 800, 200), ('A_600M_25.bseq', 1600, 150),
+            ('A_15M_1500.bseq', 800, 3), ('A_50M_200.bseq', 920, 3)]
+
+dirtsqr = True
+if dirtsqr:
+  for i, matrix in enumerate(matrices):
+    if i != 7: continue
+    cmd = 'python run_rec_dirtsqr.py '
+    cmd += '--input=%s ' % matrix[0]
+    ncols = matrix[0].strip('.bseq').split('_')[2]
+    cmd += '--ncols=%s ' % ncols
+    cmd += '--schedule=%d,%d,%d ' % (matrix[1], matrix[1], matrix[1])
+    cmd += '--times_output=dirtsqr_recursive_perf '
+    cmd += '--output=DIRTSQR_PERF_TESTING '
+    cmd += '--hadoop=icme-hadoop1 '
+    cm.exec_cmd(cmd)
